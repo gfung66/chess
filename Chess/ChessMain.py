dd2c69de52e193cebc182a1db9d8700d4601cc82
gfung66 <101066090+gfung66@users.noreply.github.com>
2022-03-09 22:30:15 -0800

    Initial commit

---
+++ Chess/ChessMain.py
@@ -0,0 +1,95 @@
+"""
+Main driver file.
+Handling user input.
+Displaying current GameStatus object.
+"""
+import pygame as p
+import ChessEngine
+
+BOARD_WIDTH = BOARD_HEIGHT = 512
+DIMENSION = 8
+SQUARE_SIZE = BOARD_HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+"""
+Initialize a global directory of images.
+This will be called exactly once in the main.
+"""
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+
+"""
+The main driver for our code.
+This will handle user input and updating the graphics.
+"""
+def main():
+    p.init()
+    screen = p.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    game_state = ChessEngine.GameState()
+    loadImages()  # do this only once before while loop
+    running = True
+    square_selected = ()  # no square is selected initially, this will keep track of the last click of the user (tuple(row,col))
+    player_clicks = []  # this will keep track of player clicks (two tuples)
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            # mouse handler
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x, y) location of the mouse
+                col = location[0] // SQUARE_SIZE
+                row = location[1] // SQUARE_SIZE
+                if square_selected == (row, col):  # user clicked the same square twice
+                    square_selected = ()  # deselect
+                    player_clicks = []  # clear clicks
+                else:
+                    square_selected = (row, col)
+                    player_clicks.append(square_selected)  # append for both 1st and 2nd click
+                    if len(player_clicks) == 2:  # after 2nd click
+                        move = ChessEngine.Move(player_clicks[0], player_clicks[1], game_state.board)
+                        print(move.getChessNotation())
+                        game_state.makeMove(move)
+                        square_selected = () #reset user clicks
+                        player_clicks = []
+
+        drawGameState(screen, game_state)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+"""
+Responsible for all the graphics within current game state.
+"""
+def drawGameState(screen, game_state):
+    drawBoard(screen)  # draw squares on the board
+    drawPieces(screen, game_state.board)  # draw pieces on top of those squares
+
+"""
+Draw the squares on the board.
+The top left square is always light.
+"""
+def drawBoard(screen):
+    global colors
+    colors = [p.Color("white"), p.Color("gray")]
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            color = colors[((row + column) % 2)]
+            p.draw.rect(screen, color, p.Rect(column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+"""
+Draw the pieces on the board using the current game_state.board
+"""
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            piece = board[row][column]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+if __name__ == "__main__":
+    main()
